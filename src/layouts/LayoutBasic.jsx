bf97b901711b3bb860c8a90ede7a671934d7bfad
Andres Acosta <andresakosta@outlook.com>
2020-09-17 23:07:17 -0500

    Routes public finished

--- src/layouts/LayoutBasic.jsx
+++ src/layouts/LayoutBasic.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import { Layout } from "antd";
+import { Route } from "react-router-dom";
+
+import "./LayoutBasic.scss";
 const LayoutBasic = ({ routes }) => {
+  const { Content, Footer } = Layout;
+
   return (
     <Layout>
-      <h2>Menu Sidbar Public</h2>
-      <div>{JSON.stringify(routes)}</div>
-      <h5>Footer</h5>
+      <h2>Menu ....</h2>
+      <Layout>
+        <Content>
+          <LoadRoutes routes={routes} />
+        </Content>
+        <Footer>Andy Acosta</Footer>
+      </Layout>
     </Layout>
   );
 };
-
+function LoadRoutes({ routes }) {
+  return routes.map((route, index) => (
+    <Route
+      key={index}
+      path={route.path}
+      exact={route.exact}
+      component={route.component}
+    />
+  ));
+}
 export default LayoutBasic;
