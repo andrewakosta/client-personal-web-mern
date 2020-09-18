702277749132e364714fbea0a5e2f656fc689b28
Andres Acosta <andresakosta@outlook.com>
2020-09-17 20:29:29 -0500

    Layouts admin and basics added

---
+++ src/layouts/LayoutBasic.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Layout } from "antd";
+const LayoutBasic = () => {
+  return (
+    <Layout>
+      <h2>Menu Sidbar</h2>
+      <div>Contact....</div>
+      <h5>Footer</h5>
+    </Layout>
+  );
+};
+
+export default LayoutBasic;
